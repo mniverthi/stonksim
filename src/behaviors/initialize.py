23203b9ca09046d1edf7f9fb5c5db6bb6fb1dce6
mniverthi <manojniverthi@gmail.com>
2021-04-13 21:51:26 -0400

    reworked globals and initialization

--- src/behaviors/initialize.py
+++ src/behaviors/initialize.py
@@ -12,55 +12,60 @@ def behavior(state, context):
         "behaviors": ["@hash/counter/counter.rs", "market.py"],
         "counter": 0,
         "sells": [],
-        "buys": []
+        "buys": [],
+        "current_price": props["initial_price"]
     }
     retail_traders = [dict({
+        "agent_name": "retail" + str(ind),
         "agent_type": "retail",
         "behaviors": ["@hash/counter/counter.rs", "sell.py", "buy.py"], # TODO: update the correct behaviors
         #"position": [i, 3],
         "shares": round(random.uniform(0, props["max_starting_shares"])),
         "counter": 0,
-        "capital": props["retail_capital"],
+        "capital": round(random.gauss(props["retail_capital"], 100)),
         "profit_threshold": props["retail_profit"],
         "loss_threshold": props["retail_sell"],
         "buy_threshold": props["retail_buy"]
     }) for ind in range(int(props["retail_count"]))]
 
     wsb_traders = [dict({
+        "agent_name": "wsb" + str(ind),
         "agent_type": "wsb",
         "behaviors": ["@hash/counter/counter.rs", "sell.py", "buy.py"], # TODO: update the correct behaviors
         # "position": [i, 4],
         "shares": round(random.uniform(0, props["max_starting_shares"])),
         "counter": 0,
-        "capital": props["wsb_capital"],
+        "capital": random.gauss(props["wsb_capital"], 100),
         "profit_threshold": props["wsb_profit"],
         "loss_threshold": props["wsb_sell"],
         "buy_threshold": props["wsb_buy"]
     }) for ind in range(int(props["wsb_count"]))]
 
     hedge_funds = [dict({
+        "agent_name": "hedge" + str(ind),
         "agent_type": "hedge",
         "behaviors": ["@hash/counter/counter.rs", "sell.py", "buy.py"], # TODO: update the correct behaviors
         #"position": [i, 5],
-        "shorts": props["hedge_short_amount"],
+        "shares": props["hedge_short_amount"],
         "counter": 0,
         # "price": 1,
         # "cost": 0,
-        "capital": props["hedge_capital"],
+        "capital": round(random.gauss(props["hedge_capital"], 100)),
         "profit_threshold": props["hedge_profit"],
         "loss_threshold": props["hedge_sell"],
         "buy_threshold": props["hedge_buy"]
     }) for ind in range(int(props["hedge_count"]))]
 
     melvin_funds = [dict({
+        "agent_name": "melvin" + str(ind), 
         "agent_type": "melvin",
         "behaviors": ["@hash/counter/counter.rs", "sell.py", "buy.py"], # TODO: update the correct behaviors
         #"position": [i, 6],
-        "shorts": props["melvin_short_amount"],
+        "shares": props["melvin_short_amount"],
         "counter": 0,
         # "price": 1,
         # "cost": 0,
-        "capital": props["melvin_capital"],
+        "capital": round(random.gauss(props["melvin_capital"], 100)),
         "profit_threshold": props["melvin_profit"],
         "loss_threshold": props["melvin_sell"],
         "buy_threshold": props["melvin_buy"]
@@ -70,4 +75,3 @@ def behavior(state, context):
     for i, agent in enumerate(agents):
         state.add_message("hash", "create_agent", agent)
     state.add_message("hash", "remove_agent")
-    
